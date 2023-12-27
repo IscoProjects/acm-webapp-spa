43f95d4b85f5068be6016b545b9b7ea13772daf5
IscoProjects <iscointriago@gmail.com>
2023-12-26 19:58:49 -0500

    Beta Web App 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,8 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'acm-project-webapp';
-
   get currentProfesional() {
     return this.authService.currentUser;
   }
